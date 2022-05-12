11213dd005102e1b0900665af092702aed6160c7
Tariq15994 <tariqnawaz15994@gmail.com>
2022-05-12 11:29:55 +0500

    Update deploy.ts
    attach with deployed address

--- PakoCoin/scripts/deploy.ts
+++ PakoCoin/scripts/deploy.ts
@@ -3,23 +3,25 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { AbiCoder } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+
 
 async function main() {
   const [owner, addr1, addr2 ] = await ethers.getSigners()
-  const PakoToken = await ethers.getContractFactory("PakoToken");
-  // const pakoToken = await PakoToken.attach("0x7422858126598B08b2b41bB79D2Cf167D0300196");
-  const pakoToken = await PakoToken.deploy();
+  const PakoToken = await ethers.getContractFactory("PakoToken");  
+  const pakoToken = await PakoToken.attach("0x664a5aCf2acAa8bbc9F7302E4EC933A5E19E1d95");
   // await pakoToken.deployed();
 
   console.log("Greeter deployed to:", pakoToken.address);
-  const balanceOfOwner =await pakoToken.balanceOf(await pakoToken.getOwner());
+  //balance of owenr
+  const balanceOfOwner =await pakoToken.balanceOf(await pakoToken.owner());
   console.log(balanceOfOwner);
+  //initial Supply 
   const initialSupply= await pakoToken._initialSupply();
-  console.log("initial Supply", initialSupply)
-  
+  console.log("initial Supply", initialSupply);  
+
 }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
